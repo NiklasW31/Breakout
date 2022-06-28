90e2c046d1a64aaff18b96dc681c84f43c1a3341
21meaa <21meaa1bif@hft-stuttgart.de>
2022-06-28 23:56:31 +0200

    änderungen: hab Block ausversehen gelöscht (habs neu gemacht) jetzt ist das Grün keine Ahnung wieso sry

--- src/game/Block.java
+++ src/game/Block.java
@@ -8,8 +8,10 @@ public class Block {
     int y;
     Pixel[][] spielfeld;
     enum Typ{typ1, typ2, typ3}
+
     Typ typ;
     boolean visibility;
+    int hits;
 
 
     Block(int x, int y, Pixel[][] spielfeld, Typ typ){
@@ -17,14 +19,32 @@ public class Block {
         this.y = y;
         this.spielfeld = spielfeld;
         this.typ = typ;
+
+        switch (typ){
+            case typ1:
+                hits = 1;
+                break;
+            case typ2:
+                hits = 2;
+                break;
+            case typ3:
+                hits = -1;
+                break;
+
+        }
     }
 
     public void drawBlock(){
-        for(int i = 0; i < 11; i++){
-            spielfeld[x + i][y].backColor = TextColor.Indexed.fromRGB(44, 244, 22);
+        if(visibility){
+            for(int i = 0; i < 11; i++){
+                spielfeld[x + i][y].backColor = TextColor.Indexed.fromRGB(44, 244, 22);
+            }
         }
+
     }
     public void visibility(){
-
+        if(hits == 0){
+            visibility = false;
+        }
     }
 }
